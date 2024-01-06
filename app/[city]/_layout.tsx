d29808f87ad25f5316c3a587c24547bc1137c8e1
Elst <jesse.elst@capgemini.com>
2024-01-06 15:43:15 +0100

    add styling to tabs

--- app/[city]/_layout.tsx
+++ app/[city]/_layout.tsx
@@ -1,3 +1,6 @@
+import Colors from "@/constants/Colors";
+import Sizes from "@/constants/Sizes";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Tabs, useGlobalSearchParams, useLocalSearchParams } from "expo-router";
 import { StatusBar } from "react-native";
 
@@ -6,12 +9,41 @@ export default function TabLayout() {
   return (
     <>
       <StatusBar barStyle={"light-content"} />
-      <Tabs screenOptions={{ headerShown: false }}>
-        <Tabs.Screen name="index" options={{ title: "Hourly" }} />
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarStyle: { height: 70 },
+          tabBarLabelStyle: {
+            fontSize: 12,
+            fontWeight: "bold",
+            marginBottom: 10,
+          },
+        }}
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Hourly",
+            tabBarIcon: ({ focused }) => (
+              <MaterialCommunityIcons
+                name={focused ? "clock" : "clock-outline"}
+                size={Sizes.icon}
+                color={Colors.primary}
+              />
+            ),
+          }}
+        />
         <Tabs.Screen
           name="days"
           options={{
             title: "Days",
+            tabBarIcon: ({ focused }) => (
+              <MaterialCommunityIcons
+                name={focused ? "calendar-refresh" : "calendar-refresh-outline"}
+                size={Sizes.icon}
+                color={Colors.primary}
+              />
+            ),
           }}
           initialParams={{ city: city }}
         />
